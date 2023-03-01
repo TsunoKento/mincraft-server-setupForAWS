d665cd350b1418884d4ade63d16d3d9ed4506282
TsunoKento <jinyejianren@gmail.com>
2023-03-01 22:57:18 +0900

    subnetの名前を変更

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,13 +6,13 @@ resource "aws_vpc" "mincraft_vpc" {
   }
 }
 
-resource "aws_subnet" "mincraft_public_1a_sn" {
+resource "aws_subnet" "mincraft_public_subnet" {
   vpc_id            = aws_vpc.mincraft_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-northeast-1a"
 
   tags = {
-    Name = "mincraft-public-1a-sn"
+    Name = "mincraft-public-subnet"
   }
 }
 
@@ -22,4 +22,3 @@ resource "aws_internet_gateway" "mincraft_igw" {
     Name = "mincraft-igw"
   }
 }
-
