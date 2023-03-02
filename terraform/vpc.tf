9063e259118fc587c983e6096c6ee924469c9347
TsunoKento <jinyejianren@gmail.com>
2023-03-02 21:47:03 +0900

    route_tableを追加

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -18,7 +18,27 @@ resource "aws_subnet" "mincraft_public_subnet" {
 
 resource "aws_internet_gateway" "mincraft_igw" {
   vpc_id = aws_vpc.mincraft_vpc.id
+
   tags = {
     Name = "mincraft-igw"
   }
+}
+
+resource "aws_route_table" "mincraft_route_table" {
+  vpc_id = aws_vpc.mincraft_vpc.id
+
+  tags = {
+    "Name" = "mincraft_route_table"
+  }
+}
+
+resource "aws_route" "mincraft_route" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.mincraft_route_table.id
+  gateway_id             = aws_internet_gateway.mincraft_igw.id
+}
+
+resource "aws_route_table_association" "route_association_public_subnet" {
+  subnet_id      = aws_subnet.mincraft_public_subnet.id
+  route_table_id = aws_route_table.mincraft_route_table.id
 }
